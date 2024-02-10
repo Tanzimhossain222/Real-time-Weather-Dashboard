d9bc2c0009920a396b17a3dee01d3686740d46f3
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-02-10 18:44:40 +0600

    Added background image based on searched city weather

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 import WeatherBoard from "./components/weather/weatherBoard";
+import { useWeatherContext } from "./context";
+import getBackground from "./utils/getBackground";
 
 const Page = () => {
+  const { loading, weatherData } = useWeatherContext();
+
+  const [climateImage, setClimateImage] = useState("");
+
+  useEffect(() => {
+    const bgImage = getBackground(weatherData.climate);
+    setClimateImage(bgImage);
+  }, [weatherData.climate]);
+
   return (
-    <div className="grid place-items-center h-screen">
-      <Header />
-      <main>
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <>
+      {loading.state ? (
+        <div className="flex bg-gray-200 rounded-md w-96 p-8 mt-14 mx-auto">
+          <p className="text-center text-3xl text-black">{loading.message}</p>
+        </div>
+      ) : (
+        <div
+          style={{ backgroundImage: `url(${climateImage})` }}
+          className="grid place-items-center h-screen bg-no-repeat bg-cover"
+        >
+          <Header />
+          <main>
+            <section>
+              <WeatherBoard />
+            </section>
+          </main>
+        </div>
+      )}
+    </>
   );
 };
 
